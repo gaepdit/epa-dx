9f874dddea614c2832bf47af774f0e9bb8f17e61
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-02-09 17:16:35 -0500

    Update DX stats queries

--- Data-tests/Count all ICIS data.sql
+++ Data-tests/Count all ICIS data.sql
@@ -1,45 +1,61 @@
---/* Count unprocessed data 
+--/* Count unprocessed updates
 
-USE airbranch;
-GO
+SELECT 'APBSUPPLAMENTALDATA' as Data, SUM(IIF(ICIS_STATUSIND <> 'P', 1, 0)) AS Unprocessed, COUNT(*) AS Total FROM AIRBRANCH.dbo.APBSUPPLAMENTALDATA
+union
+SELECT 'APBFACILITYINFORMATION' as Data, SUM(IIF(ICIS_STATUSIND <> 'P', 1, 0)) AS Unprocessed, COUNT(*) AS Total FROM AIRBRANCH.dbo.APBFACILITYINFORMATION
+union
+SELECT 'APBHEADERDATA' as Data, SUM(IIF(ICIS_STATUSIND <> 'P', 1, 0)) AS Unprocessed, COUNT(*) AS Total FROM AIRBRANCH.dbo.APBHEADERDATA
+union
+select 'Fces' as Data, sum(iif(DataExchangeStatus = 'U', 1, 0)) as Unprocessed, count(*) as Total from AirWeb.dbo.Fces where IsDeleted = 0 and ActionNumber is not null
+union
+select 'ComplianceWork' as Data, sum(iif(DataExchangeStatus = 'U', 1, 0)) as Unprocessed, count(*) as Total from AirWeb.dbo.ComplianceWork where IsDeleted = 0 and ActionNumber is not null
+union
+select 'EnforcementActions' as Data, sum(iif(DataExchangeStatus = 'U', 1, 0)) as Unprocessed, count(*) as Total from AirWeb.dbo.EnforcementActions where IsDeleted = 0 and ActionNumber is not null
+union
+select 'CaseFiles' as Data, sum(iif(DataExchangeStatus = 'U', 1, 0)) as Unprocessed, count(*) as Total from AirWeb.dbo.CaseFiles where IsDeleted = 0 and ActionNumber is not null
 
-SELECT SUM(CASE WHEN ICIS_STATUSIND <> 'P' THEN 1 ELSE 0 END) AS Unprocessed, COUNT(*) AS ICIS_DELETE FROM airbranch.dbo.ICIS_DELETE ;
-SELECT SUM(CASE WHEN ICIS_STATUSIND <> 'P' THEN 1 ELSE 0 END) AS Unprocessed, COUNT(*) AS SSCP_AUDITEDENFORCEMENT FROM airbranch.dbo.SSCP_AUDITEDENFORCEMENT;
-SELECT SUM(CASE WHEN ICIS_STATUSIND <> 'P' THEN 1 ELSE 0 END) AS Unprocessed, COUNT(*) AS SSCPFCEMASTER FROM airbranch.dbo.SSCPFCEMASTER;
-SELECT SUM(CASE WHEN ICIS_STATUSIND <> 'P' THEN 1 ELSE 0 END) AS Unprocessed, COUNT(*) AS SSCPITEMMASTER FROM airbranch.dbo.SSCPITEMMASTER;
-SELECT SUM(CASE WHEN ICIS_STATUSIND <> 'P' THEN 1 ELSE 0 END) AS Unprocessed, COUNT(*) AS APBSUPPLAMENTALDATA FROM airbranch.dbo.APBSUPPLAMENTALDATA;
-SELECT SUM(CASE WHEN ICIS_STATUSIND <> 'P' THEN 1 ELSE 0 END) AS Unprocessed, COUNT(*) AS APBFACILITYINFORMATION FROM airbranch.dbo.APBFACILITYINFORMATION;
-SELECT SUM(CASE WHEN ICIS_STATUSIND <> 'P' THEN 1 ELSE 0 END) AS Unprocessed, COUNT(*) AS APBHEADERDATA FROM airbranch.dbo.APBHEADERDATA;
-SELECT SUM(CASE WHEN ICIS_STATUSIND <> 'P' THEN 1 ELSE 0 END) AS Unprocessed, COUNT(*) AS VW_ICIS_COMPLIANCEMONITORING FROM airbranch.dbo.VW_ICIS_COMPLIANCEMONITORING;
-SELECT SUM(CASE WHEN ICIS_STATUSIND <> 'P' THEN 1 ELSE 0 END) AS Unprocessed, COUNT(*) AS VW_ICIS_CASEFILE FROM airbranch.dbo.VW_ICIS_CASEFILE;
-SELECT SUM(CASE WHEN ICIS_STATUSIND <> 'P' THEN 1 ELSE 0 END) AS Unprocessed, COUNT(*) AS VW_ICIS_ENFORCEMENTACTION FROM airbranch.dbo.VW_ICIS_ENFORCEMENTACTION;
-
+;
 --*/
---/* Count staged records 
+--/* Count unprocessed deletions
 
-USE NETWORKNODEFLOW;
+SELECT 'ICIS_DELETE' as Data, SUM(IIF(ICIS_STATUSIND <> 'P', 1, 0)) AS Unprocessed, COUNT(*) AS Total FROM AIRBRANCH.dbo.ICIS_DELETE
+union
+select 'Fces' as Data, sum(iif(DataExchangeStatus = 'D', 1, 0)) as Unprocessed, count(*) as Total from AirWeb.dbo.Fces where ActionNumber is not null
+union
+select 'ComplianceWork' as Data, sum(iif(DataExchangeStatus = 'D', 1, 0)) as Unprocessed, count(*) as Total from AirWeb.dbo.ComplianceWork where ActionNumber is not null
+union
+select 'EnforcementActions' as Data, sum(iif(DataExchangeStatus = 'D', 1, 0)) as Unprocessed, count(*) as Total from AirWeb.dbo.EnforcementActions where ActionNumber is not null
+union
+select 'CaseFiles' as Data, sum(iif(DataExchangeStatus = 'D', 1, 0)) as Unprocessed, count(*) as Total from AirWeb.dbo.CaseFiles where ActionNumber is not null
+;
+--*/
+--/* Count staged records
+
+USE NETWORKNODEFLOW
 GO
 
 SELECT COUNT(*) as [Unprocessed Submissions]
 FROM   dbo.SubmissionStatus
-WHERE  status IS NULL;
+WHERE  Status IS NULL;
 
-CREATE TABLE #counts
-(
-             table_name varchar(255)
-           , row_count  int
-);
+begin
+    CREATE TABLE #counts
+    (
+        table_name varchar(255),
+        row_count  int
+    );
 
-EXEC sp_MSForEachTable
-     @command1 = 'INSERT #counts (table_name, row_count) SELECT ''?'', COUNT(*) FROM ?';
+    EXEC sys.sp_MSforeachtable
+         @command1 = 'INSERT #counts (table_name, row_count) SELECT ''?'', COUNT(*) FROM ?';
 
-SELECT table_name
-     , row_count
-FROM   #counts
-WHERE  table_name NOT LIKE '%CERS_%'
-ORDER BY table_name
-       , row_count DESC;
+    SELECT table_name
+         , row_count
+    FROM #counts
+    WHERE table_name NOT LIKE '%CERS_%'
+    ORDER BY table_name
+           , row_count DESC;
 
-DROP TABLE #counts;
+    DROP TABLE #counts;
+end
 
 --*/
